c390e170122851b20bbf6c76bc8f17ad46508709
chalasimon <chally.r@hotmail.com>
2025-07-29 18:07:25 +0300

    feat: important libraries are imported

---
+++ src/model_explaining/shap_analysis.py
@@ -0,0 +1,14 @@
+# import important libraries
+import pandas as pd
+# Imports
+import shap
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.linear_model import LogisticRegression
+from lightgbm import LGBMClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
